918b861bd643ceaf996b745c09ed66e911f18872
Rudolfs Lukstins <rudolfs.lukstins@prime-force.com>
2026-05-19 18:59:39 +0300

    Updates

--- server/assets/_tanstack-start-manifest_v-CmiuhMvt.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/rudol/Desktop/Mordata/New-mordata/mordata-process-flow/src/routes/__root.tsx", children: ["/", "/sitemap.xml"], assets: void 0, preloads: ["/assets/index-DgiAaZWD.js"] }, "/": { filePath: "C:/Users/rudol/Desktop/Mordata/New-mordata/mordata-process-flow/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-CVq2Y4rN.js"] } }, clientEntry: "/assets/index-DgiAaZWD.js" });
-export {
-  tsrStartManifest
-};
